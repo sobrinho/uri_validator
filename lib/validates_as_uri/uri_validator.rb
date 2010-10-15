f299c87564f856aa981386b39dc01a4b14958a34
Christian Eichhorn <ceichhor@mx.webmasters.de>
2010-10-15 14:54:13 +0200

    * Fixed bug with frozen options hash * added translation key for error message

--- lib/validates_as_uri/uri_validator.rb
+++ lib/validates_as_uri/uri_validator.rb
@@ -1,9 +1,9 @@
 class UriValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    options[:protocols] ||= %w(http https)
+    protocols = options[:protocols] || %w(http https)
     
-    if value !~ %r{^(#{Array(options[:protocols]).join('|')})://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$}
-      record.errors.add(attribute, options[:message])
+    if value !~ %r{^(#{Array(protocols).join('|')})://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$}
+      record.errors.add(attribute, options[:message] || I18n.t(:uri_invalid, :scope => [:activerecord, :errors, :messages]))
     end
   end
 end
