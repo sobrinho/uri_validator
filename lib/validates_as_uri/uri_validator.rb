982cce3a201d22978dc861a907a23e3f829626ec
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2010-10-30 17:23:40 -0200

    Use Array.wrap() instead of Array()

--- lib/validates_as_uri/uri_validator.rb
+++ lib/validates_as_uri/uri_validator.rb
@@ -2,7 +2,7 @@ class UriValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     protocols = options[:protocols] || %w(http https)
     
-    if value !~ %r{^(#{Array(protocols).join('|')})://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$}
+    if value !~ %r{^(#{Array.wrap(protocols).join('|')})://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$}
       record.errors.add(attribute, options[:message] || I18n.t(:uri_invalid, :scope => [:activerecord, :errors, :messages]))
     end
   end
