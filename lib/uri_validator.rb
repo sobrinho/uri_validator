9264bd3391f3ba226ab83ab0ed7734b9fa3f7aa0
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2011-07-22 17:55:23 -0300

    Move uri_validator/uri_validator.rb to uri_validator.rb

--- lib/uri_validator.rb
+++ lib/uri_validator.rb
@@ -1,1 +1,9 @@
-require 'uri_validator/uri_validator'
+class UriValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    protocols = options[:protocols] || %w(http https)
+
+    if value !~ %r{^(#{Array.wrap(protocols).join('|')})://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$}
+      record.errors.add(attribute, options[:message])
+    end
+  end
+end
--- lib/uri_validator/uri_validator.rb
+++
@@ -1,9 +0,0 @@
-class UriValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    protocols = options[:protocols] || %w(http https)
-    
-    if value !~ %r{^(#{Array.wrap(protocols).join('|')})://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$}
-      record.errors.add(attribute, options[:message])
-    end
-  end
-end
