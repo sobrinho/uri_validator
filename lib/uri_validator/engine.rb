ec4d2bd5dfa74693b6d649d7463e14f7f781092b
Gabriel Sobrinho <gabriel.sobrinho@gmail.com>
2011-07-22 17:51:14 -0300

    Use default message for validation

--- lib/uri_validator/engine.rb
+++
@@ -1,4 +0,0 @@
-module ValidateAsUri
-  class Engine < Rails::Engine
-  end
-end
--- lib/uri_validator/uri_validator.rb
+++ lib/uri_validator/uri_validator.rb
@@ -3,7 +3,7 @@ class UriValidator < ActiveModel::EachValidator
     protocols = options[:protocols] || %w(http https)
     
     if value !~ %r{^(#{Array.wrap(protocols).join('|')})://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$}
-      record.errors.add(attribute, options[:message] || :invalid_uri)
+      record.errors.add(attribute, options[:message])
     end
   end
 end
