54b99e2be8762d74773e1243a72e64f018749b57
Herminio Torres <herminiocesar@gmail.com>
2012-01-18 23:00:06 -0200

    this way turn more simple, read this code or you choice in regex using #{protocols.join("|")}

--- lib/uri_validator.rb
+++ lib/uri_validator.rb
@@ -1,8 +1,10 @@
 class UriValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
-    protocols = options[:protocols] || %w(http https)
-
-    if value !~ %r{^(#{Array.wrap(protocols).join('|')})://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$}
+  
+    protocols = options[:protocols].join('|') || %w(http https).join('|')
+    
+    # check if value not match regex is true return error, example protocols "http|https"
+    if value !~ %r{^(#{protocols})://[a-z0-9-]+(.[a-z0-9-]+)*(:[0-9]+)?(/.*)?$}
       record.errors.add(attribute, options[:message])
     end
   end
